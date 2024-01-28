5e2fc299a63fe70b87335faa78d8aebe52cd3837
choilhyun <cihg155@gmail.com>
2024-01-28 13:42:02 +0900

    최초 구성

---
+++ src/main/java/com/example/guidejwt/GuideJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.guidejwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuideJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuideJwtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/guidejwt/GuideJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.guidejwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuideJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
